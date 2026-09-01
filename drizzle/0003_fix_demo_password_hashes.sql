e9cad9e540919cef59222389b8af7098c972446e
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2026-09-01 10:28:34 +0800

    Fix production login verification hashes

---
+++ drizzle/0003_fix_demo_password_hashes.sql
@@ -0,0 +1,3 @@
+UPDATE `app_users` SET `password_hash` = '25b53864a7690958f338660b16dddcfa9863542d32e95a9c833afa38c143135c' WHERE `id` = 'demo-user-abc';--> statement-breakpoint
+UPDATE `app_users` SET `password_hash` = 'fdf5769977ad6400fdfda0e7e8ff43212e36a167fd281d7d39ece891c5fb114a' WHERE `id` = 'demo-user-cde';--> statement-breakpoint
+UPDATE `app_users` SET `password_hash` = '4f9901a25c7bb7816780301a95b3a9f31fe415c8249e9159b5b9431a04ed3ded' WHERE `id` = 'demo-user-fgh';
